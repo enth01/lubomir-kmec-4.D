6177798310e0ca25cb18272252ffbaca09625eaa
enth01 <kmec7896@gmail.com>
2025-09-11 11:05:47 +0200

    uloha1

---
+++ WebApplication1/Controllers/Ulohy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Ulohy : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+    }
+}
